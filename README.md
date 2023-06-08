c0ef95e6da740ea9ae496c435641a443719fb9ef
melikbugra <melikbugraozcelik@gmail.com>
2023-06-09 01:55:18 +0300

    rainbow implemented

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 <img src="Vanilla_DQN.png" width="50%" height="50%"><img src="Double_DQN.png" width="50%" height="50%">
 <img src="PER_DQN.png" width="50%" height="50%"><img src="Dueling_DQN.png" width="50%" height="50%">
 <img src="Noisy_DQN.png" width="50%" height="50%"><img src="Categorical_DQN.png" width="50%" height="50%">
-<img src="N-Step_DQN.png" width="50%" height="50%">
-
+<img src="N-Step_DQN.png" width="50%" height="50%"><img src="Rainbow_DQN.png" width="50%" height="50%">
---
+++ rainbow/dqn/replay_memory.py
@@ -0,0 +1,347 @@
+from collections import namedtuple, deque
+import torch
+import numpy as np
+from typing import Dict, Deque, Tuple, List
+import random
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward', 'done'))
+
+
+class ReplayMemory:
+    """A simple numpy replay bufferfer."""
+
+    def __init__(self, state_dim: int, action_dim: int, size: int, batch_size: int = 32, n_step: int = 5, gamma: float = 0.99):
+        self.state_buffer = torch.zeros([size, 1, state_dim], dtype=torch.float32)
+        self.next_state_buffer = torch.zeros([size, 1, state_dim], dtype=torch.float32)
+        self.action_buffer = torch.zeros([size, 1, action_dim], dtype=torch.int64)
+        self.reward_buffer = torch.zeros([size, 1, 1], dtype=torch.float32)
+        self.done_buffer = torch.zeros([size, 1, 1], dtype=torch.bool)
+        self.max_size, self.batch_size = size, batch_size
+        self.ptr, self.size, = 0, 0
+
+        # for N-step Learning
+        self.n_step_buffer = deque(maxlen=n_step)
+        self.n_step = n_step
+        self.gamma = gamma
+
+
+    def push(self, transition: Transition):
+        self.n_step_buffer.append(transition)
+
+        # single step transition is not ready
+        if len(self.n_step_buffer) < self.n_step:
+            return ()
+        
+        # make a n-step transition
+        reward, next_state, done = self._get_n_step_info(
+            self.n_step_buffer, self.gamma
+        )
+        state = self.n_step_buffer[0].state
+        action = self.n_step_buffer[0].action
+
+        self.state_buffer[self.ptr] = state
+        self.next_state_buffer[self.ptr] = next_state
+        self.action_buffer[self.ptr] = action
+        self.reward_buffer[self.ptr] = reward
+        self.done_buffer[self.ptr] = done
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+        return self.n_step_buffer[0]
+
+    def sample(self) -> Dict[str, np.ndarray]:
+        idxs = np.random.choice(self.size, size=self.batch_size, replace=False)
+        state=self.state_buffer[idxs],
+        action=self.action_buffer[idxs],
+        next_state=self.next_state_buffer[idxs],
+        reward=self.reward_buffer[idxs],
+        done=self.done_buffer[idxs]
+        batch = Transition(state=state,
+                            action=action,
+                            next_state=next_state,
+                            reward=reward,
+                            done=done)
+        
+        return batch, idxs
+    
+    def sample_batch_from_idxs(
+        self, indices: np.ndarray
+    ) -> Dict[str, np.ndarray]:
+        state=self.state_buffer[indices],
+        action=self.action_buffer[indices],
+        next_state=self.next_state_buffer[indices],
+        reward=self.reward_buffer[indices],
+        done=self.done_buffer[indices]
+        batch = Transition(state=state,
+                            action=action,
+                            next_state=next_state,
+                            reward=reward,
+                            done=done)
+
+        return batch
+        
+    def _get_n_step_info(
+        self, n_step_buffer: Deque, gamma: float
+    ) -> Tuple[np.int64, np.ndarray, bool]:
+        """Return n step rew, next_obs, and done."""
+        # info of the last transition
+        reward = n_step_buffer[-1].reward
+        next_state = n_step_buffer[-1].next_state
+        done = n_step_buffer[-1].done
+
+        for transition in reversed(list(n_step_buffer)[:-1]):
+            r = transition.reward
+            n_s = transition.next_state
+            d = transition.done
+
+            reward = r + gamma * reward * (1 - d)
+            next_state, done = (n_s, d) if d else (next_state, done)
+
+        return reward, next_state, done
+
+    def __len__(self) -> int:
+        return self.size
+    
+
+class PrioritizedReplayMemory(ReplayMemory):
+    """Prioritized Replay buffer.
+    
+    Attributes:
+        max_priority (float): max priority
+        tree_ptr (int): next index of tree
+        alpha (float): alpha parameter for prioritized replay buffer
+        sum_tree (SumSegmentTree): sum tree for prior
+        min_tree (MinSegmentTree): min tree for min prior to get max weight
+        
+    """
+    
+    def __init__(self, state_dim: int, action_dim: int, size: int, batch_size: int, alpha: float):
+        """Initialization."""
+        assert alpha >= 0
+        
+        super().__init__(state_dim, action_dim, size, batch_size)
+        self.max_priority, self.tree_ptr = 1.0, 0
+        self.alpha = alpha
+        
+        # capacity must be positive and a power of 2.
+        tree_capacity = 1
+        while tree_capacity < self.max_size:
+            tree_capacity *= 2
+
+        self.sum_tree = SumSegmentTree(tree_capacity)
+        self.min_tree = MinSegmentTree(tree_capacity)
+        
+    def push(self, transition: Transition):
+        """Store experience and priority."""
+        _transition = super().push(transition)
+        
+        if _transition:
+            self.sum_tree[self.tree_ptr] = self.max_priority ** self.alpha
+            self.min_tree[self.tree_ptr] = self.max_priority ** self.alpha
+            self.tree_ptr = (self.tree_ptr + 1) % self.max_size
+
+        return transition
+
+    def sample(self, beta: float = 0.4) -> Dict[str, np.ndarray]:
+        """Sample a batch of experiences."""
+        assert len(self) >= self.batch_size
+        assert beta > 0
+        
+        indices = self._sample_proportional()
+        
+        state=self.state_buffer[indices],
+        action=self.action_buffer[indices],
+        next_state=self.next_state_buffer[indices],
+        reward=self.reward_buffer[indices],
+        done=self.done_buffer[indices]
+        batch = Transition(state=state,
+                            action=action,
+                            next_state=next_state,
+                            reward=reward,
+                            done=done)
+        weights = np.array([self._calculate_weight(i, beta) for i in indices])
+        
+        return batch, weights, indices
+        
+    def update_priorities(self, indices: List[int], priorities: np.ndarray):
+        """Update priorities of sampled transitions."""
+        assert len(indices) == len(priorities)
+
+        for idx, priority in zip(indices, priorities):
+            assert priority > 0
+            assert 0 <= idx < len(self)
+
+            self.sum_tree[idx] = priority ** self.alpha
+            self.min_tree[idx] = priority ** self.alpha
+
+            self.max_priority = max(self.max_priority, priority)
+            
+    def _sample_proportional(self) -> List[int]:
+        """Sample indices based on proportions."""
+        indices = []
+        p_total = self.sum_tree.sum(0, len(self) - 1)
+        segment = p_total / self.batch_size
+        
+        for i in range(self.batch_size):
+            a = segment * i
+            b = segment * (i + 1)
+            upperbound = random.uniform(a, b)
+            idx = self.sum_tree.retrieve(upperbound)
+            indices.append(idx)
+            
+        return indices
+    
+    def _calculate_weight(self, idx: int, beta: float):
+        """Calculate the weight of the experience at idx."""
+        # get max weight
+        p_min = self.min_tree.min() / self.sum_tree.sum()
+        max_weight = (p_min * len(self)) ** (-beta)
+        
+        # calculate weights
+        p_sample = self.sum_tree[idx] / self.sum_tree.sum()
+        weight = (p_sample * len(self)) ** (-beta)
+        weight = weight / max_weight
+        
+        return weight
+    
+
+import operator
+from typing import Callable
+
+
+class SegmentTree:
+    """ Create SegmentTree.
+
+    Taken from OpenAI baselines github repository:
+    https://github.com/openai/baselines/blob/master/baselines/common/segment_tree.py
+
+    Attributes:
+        capacity (int)
+        tree (list)
+        operation (function)
+
+    """
+
+    def __init__(self, capacity: int, operation: Callable, init_value: float):
+        """Initialization.
+
+        Args:
+            capacity (int)
+            operation (function)
+            init_value (float)
+
+        """
+        assert (
+            capacity > 0 and capacity & (capacity - 1) == 0
+        ), "capacity must be positive and a power of 2."
+        self.capacity = capacity
+        self.tree = [init_value for _ in range(2 * capacity)]
+        self.operation = operation
+
+    def _operate_helper(
+        self, start: int, end: int, node: int, node_start: int, node_end: int
+    ) -> float:
+        """Returns result of operation in segment."""
+        if start == node_start and end == node_end:
+            return self.tree[node]
+        mid = (node_start + node_end) // 2
+        if end <= mid:
+            return self._operate_helper(start, end, 2 * node, node_start, mid)
+        else:
+            if mid + 1 <= start:
+                return self._operate_helper(start, end, 2 * node + 1, mid + 1, node_end)
+            else:
+                return self.operation(
+                    self._operate_helper(start, mid, 2 * node, node_start, mid),
+                    self._operate_helper(mid + 1, end, 2 * node + 1, mid + 1, node_end),
+                )
+
+    def operate(self, start: int = 0, end: int = 0) -> float:
+        """Returns result of applying `self.operation`."""
+        if end <= 0:
+            end += self.capacity
+        end -= 1
+
+        return self._operate_helper(start, end, 1, 0, self.capacity - 1)
+
+    def __setitem__(self, idx: int, val: float):
+        """Set value in tree."""
+        idx += self.capacity
+        self.tree[idx] = val
+
+        idx //= 2
+        while idx >= 1:
+            self.tree[idx] = self.operation(self.tree[2 * idx], self.tree[2 * idx + 1])
+            idx //= 2
+
+    def __getitem__(self, idx: int) -> float:
+        """Get real value in leaf node of tree."""
+        assert 0 <= idx < self.capacity
+
+        return self.tree[self.capacity + idx]
+
+
+class SumSegmentTree(SegmentTree):
+    """ Create SumSegmentTree.
+
+    Taken from OpenAI baselines github repository:
+    https://github.com/openai/baselines/blob/master/baselines/common/segment_tree.py
+
+    """
+
+    def __init__(self, capacity: int):
+        """Initialization.
+
+        Args:
+            capacity (int)
+
+        """
+        super(SumSegmentTree, self).__init__(
+            capacity=capacity, operation=operator.add, init_value=0.0
+        )
+
+    def sum(self, start: int = 0, end: int = 0) -> float:
+        """Returns arr[start] + ... + arr[end]."""
+        return super(SumSegmentTree, self).operate(start, end)
+
+    def retrieve(self, upperbound: float) -> int:
+        """Find the highest index `i` about upper bound in the tree"""
+        # TODO: Check assert case and fix bug
+        assert 0 <= upperbound <= self.sum() + 1e-5, "upperbound: {}".format(upperbound)
+
+        idx = 1
+
+        while idx < self.capacity:  # while non-leaf
+            left = 2 * idx
+            right = left + 1
+            if self.tree[left] > upperbound:
+                idx = 2 * idx
+            else:
+                upperbound -= self.tree[left]
+                idx = right
+        return idx - self.capacity
+
+
+class MinSegmentTree(SegmentTree):
+    """ Create SegmentTree.
+
+    Taken from OpenAI baselines github repository:
+    https://github.com/openai/baselines/blob/master/baselines/common/segment_tree.py
+
+    """
+
+    def __init__(self, capacity: int):
+        """Initialization.
+
+        Args:
+            capacity (int)
+
+        """
+        super(MinSegmentTree, self).__init__(
+            capacity=capacity, operation=min, init_value=float("inf")
+        )
+
+    def min(self, start: int = 0, end: int = 0) -> float:
+        """Returns min(arr[start], ...,  arr[end])."""
+        return super(MinSegmentTree, self).operate(start, end)
