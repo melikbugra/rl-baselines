687b8e3e605994ea53149302dbd45b1209fd9134
melikbugra <melikbugraozcelik@gmail.com>
2024-02-12 01:04:08 +0300

    xdriving train

--- train.py
+++ train.py
@@ -8,16 +8,18 @@ def main():
     env = gym.make("xDriving-v0")
     model = VanillaDQN(
         env=env,
-        time_steps=5000000,
-        learning_rate=3e-4,
-        batch_size=128,
-        experience_replay_size=100000,
+        time_steps=300000,
+        learning_rate=0.00040594615247134503,
+        batch_size=256,
+        gradient_steps=3,
+        gamma=0.93,
+        experience_replay_size=10000,
         render=False,
         exploration_percentage=50,
         writing_period=10000,
         plot_train_sores=True,
         mlflow_tracking_uri="http://mlflow.melikbugraozcelik.com/",
-        normalize_observation=False,
+        normalize_observation=True,
         network_arch=[256, 256],
     )
     model.train()
